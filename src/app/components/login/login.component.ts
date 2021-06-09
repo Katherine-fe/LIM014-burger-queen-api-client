e036d00fbd2c8e8a98bc20d4f61a0dfe07dc037f
Eunice Fiorella <euniceancajima@gmail.com>
2021-06-09 13:30:57 -0500

    Mejorando mensaje de salida de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -17,44 +17,55 @@ export class LoginComponent implements OnInit {
     password: ''
   };
   error: boolean = false;
-  errorMessage: string = 'Please enter a valid email or password'
+  errorMessage: string = '';
   ngOnInit(): void { }
 
   login() {
-    this.auth.requestPost(this.user.email, this.user.password)
-      .subscribe(data => {
-        this.auth.setToken(data.token);
-        const token = this.auth.getToken();
-        switch (token) {
-          case '"tokenAdmin"':
-            this.auth.authorization().subscribe(() => {
-              if (this.auth.isLoggedIn) {
-                this.router.navigate(['menuprincipal']);
-              }
-            })
-            break;
-          case '"tokenCocinero"':
-            this.auth.authorization().subscribe(() => {
-              if (this.auth.isLoggedIn) {
-                this.router.navigate(['mainkitchener']);
-              }
-            })
-            break;
-          case '"tokenMesero"':
-            this.auth.authorization().subscribe(() => {
-              if (this.auth.isLoggedIn) {
-                this.router.navigate(['mainWaiter']);
-              }
-            })
-            break;
-          default:
-            console.log("No pasa nada");
-        }
-      }, error => {
-        if (error.status === 400) {
-          this.error = true;
-        }
-      });
+    if (this.user.email == '' || this.user.password == '') {
+      this.error = true;
+      this.errorMessage = "inputs vacios";
+    } else {
+      this.error = true;
+      this.errorMessage = "Wait a moment..."
+      this.auth.requestPost(this.user.email, this.user.password)
+        .subscribe(data => {
+          this.auth.setToken(data.token);
+          const token = this.auth.getToken();
+          switch (token) {
+            case '"tokenAdmin"':
+              this.auth.authorization().subscribe(() => {
+                if (this.auth.isLoggedIn) {
+                  this.router.navigate(['menuprincipal']);
+                }
+              })
+              break;
+            case '"tokenCocinero"':
+              this.auth.authorization().subscribe(() => {
+                if (this.auth.isLoggedIn) {
+                  this.router.navigate(['mainkitchener']);
+                }
+              })
+              break;
+            case '"tokenMesero"':
+              this.auth.authorization().subscribe(() => {
+                if (this.auth.isLoggedIn) {
+                  this.router.navigate(['mainWaiter']);
+                }
+              })
+              break;
+            default:
+              console.log("No pasa nada");
+          }
+        }, error => {
+          if (error.status === 0) {
+            this.error = true;
+            this.errorMessage = "Connection refused";
+          } else if (error.status === 400) {
+            this.error = true;
+            this.errorMessage = "Please, enter a valid email or password";
+          }
+        });
+    }
   }
 }
 
