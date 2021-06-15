12fdcae15c676b7f9cb110ddcb8153542c83c1a8
Eunice Fiorella <euniceancajima@gmail.com>
2021-06-15 00:42:35 -0500

    Listando ordenes pendientes

--- src/app/components/kitchener/pending/pending.component.ts
+++ src/app/components/kitchener/pending/pending.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
-
+import { Order } from '../../../model/order-interface';
+import { Product } from '../../../model/product-interface';
 @Component({
   selector: 'app-pending',
   templateUrl: './pending.component.html',
   styleUrls: ['./pending.component.scss']
 })
 export class PendingComponent implements OnInit {
-
+  orderList: Order[] = [];
   constructor(private get: ApiService) { }
 
   ngOnInit(): void {
@@ -16,9 +17,13 @@ export class PendingComponent implements OnInit {
 
   orders() {
     this.get.getOrders().subscribe(data => {
-      data.order.forEach((element: any) => {
-        console.log(element.client);
-      });
+      this.orderList = data.order;
     });
   }
+  prueba(id: string) {
+    return id;
+  }
+  /* return this.producto.reduce((acc, obj) => acc + obj.products[0].product.price * obj.products[0].qty,
+    0
+  ); */
 }
