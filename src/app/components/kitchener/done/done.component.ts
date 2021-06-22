43f8150b647286ae29f5937918e5599d0e4f9eb4
Eunice Fiorella <euniceancajima@gmail.com>
2021-06-22 04:45:46 -0500

    Listando ordenes cocinero

--- src/app/components/kitchener/done/done.component.ts
+++ src/app/components/kitchener/done/done.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/model/order-interface';
+import { OrdersService } from 'src/app/services/orders/orders.service';
 
 @Component({
   selector: 'app-done',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./done.component.scss']
 })
 export class DoneComponent implements OnInit {
-
-  constructor() { }
+  orderDone: Order[] = [];
+  statusCanceled: string = 'canceled';
+  constructor(private get: OrdersService) { }
 
   ngOnInit(): void {
+    this.orders();
   }
 
+  orders() {
+    this.get.getListOrders().subscribe(data => {
+      this.orderDone = data.order;
+    });
+  }
 }
--- src/app/components/kitchener/pending/pending.component.ts
+++ src/app/components/kitchener/pending/pending.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/services/api.service';
 import { Order } from '../../../model/order-interface';
 import * as dayjs from 'dayjs';
+import { OrdersService } from 'src/app/services/orders/orders.service';
 @Component({
   selector: 'app-pending',
   templateUrl: './pending.component.html',
@@ -11,8 +11,9 @@ export class PendingComponent implements OnInit {
   orderList: Order[] = [];
   filterStatus: Order[] = [];
   total!: number;
+  statusPending: string = '';
   //suscription: Subscription = new Subscription;
-  constructor(private get: ApiService) { }
+  constructor(private get: OrdersService) { }
 
 
   ngOnInit(): void {
@@ -27,9 +28,8 @@ export class PendingComponent implements OnInit {
     const date2 = dayjs();
     const result = date2.diff(date1, 'm');
     console.log(Math.trunc(result / 60), result % 60); */
-    this.get.getOrders().subscribe(data => {
+    this.get.getListOrders().subscribe(data => {
       this.orderList = data.order;
-      this.pending(this.orderList);
     });
   }
   pending(orders: any) {
@@ -41,13 +41,5 @@ export class PendingComponent implements OnInit {
     });
     //Primero por los servicios y luego mockear
     //ng module 
-  }
-  totalBill(id: string) {
-    const item = this.orderList.filter((obj) => obj._id == id)
-    this.total = item[0].products.reduce(
-      (acc, obj) => acc + obj.product.price * obj.qty,
-      0
-    );
-    return this.total;
   }
 }
