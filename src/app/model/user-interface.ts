b72c8d1ec771663f2b4296983cdcc0e10fb33ffa
Eunice Fiorella <euniceancajima@gmail.com>
2021-06-22 06:51:27 -0500

    creando interface de user

--- src/app/model/user-interface.ts
+++ src/app/model/user-interface.ts
@@ -2,3 +2,13 @@ export interface UserInterface {
     email: string;
     password: string;
 }
+
+export interface adminUser {
+    _id: string;
+    email: string;
+    password: string;
+    roles: rol
+}
+interface rol {
+    admin: boolean;
+}
